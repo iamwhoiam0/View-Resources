7b514d32f5dbf1f4f4f254d3daa6783df27a7686
iamwhoiam0 <gamzatgasanov2337@mail.ru>
2025-05-10 13:19:06 +0300

    designed the contacts activity

--- app/src/main/java/otus/gpb/homework/viewandresources/ContactsActivity.kt
+++ app/src/main/java/otus/gpb/homework/viewandresources/ContactsActivity.kt
@@ -2,10 +2,19 @@ package otus.gpb.homework.viewandresources
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import otus.gpb.homework.viewandresources.databinding.ActivityContactsBinding
 
 class ContactsActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityContactsBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_contacts)
+        setContentView(binding.root)
+
+        binding.backBtn.setOnClickListener {
+            finish()
+        }
     }
 }
